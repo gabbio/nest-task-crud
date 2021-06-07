4bdbb580f05ceb026891cf43a27614c461404f4e
Abou Bakr Gbané <aboubakr.gbane@gmail.com>
2021-06-07 11:54:18 +0000

    feat: create a task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateTaskDto } from './create-task.dto';
 import { TaskService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly taskService: TaskService) {}
+
+  @Post()
+  async create(@Body() createTaskDto: CreateTaskDto) {
+    return await this.taskService.create(createTaskDto);
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { Task } from './task.model';
+import { CreateTaskDto } from './create-task.dto';
 
 @Injectable()
 export class TaskService {
   constructor(@InjectModel(Task) private taskModel: typeof Task) {}
+
+  async create(taskDto: CreateTaskDto): Promise<Task> {
+    return await this.taskModel.create(taskDto);
+  }
 }
