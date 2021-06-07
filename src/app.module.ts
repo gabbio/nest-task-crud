4e53e3fa30790edfb3df8922a3b4e5b9e8720768
Abou Bakr Gbané <aboubakr.gbane@gmail.com>
2021-06-07 16:26:51 +0000

    chore: use ssl connections to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,9 @@ import { Task } from './tasks/task.model';
       username: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
-      ssl: true,
+      dialectOptions: {
+        ssl: true,
+      },
       autoLoadModels: true,
       models: [Task],
       synchronize: true,
