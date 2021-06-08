6187a96904483c533d64df29a8f29e7a3581bfb5
Abou Bakr Gbané <aboubakr.gbane@gmail.com>
2021-06-08 09:05:16 +0000

    docs: add OPEN API doc

--- src/tasks/create-task.dto.ts
+++ src/tasks/create-task.dto.ts
@@ -1,3 +1,6 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateTaskDto {
+  @ApiProperty({ example: 'First task', description: 'The name of the task' })
   name: string;
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,25 +9,33 @@ import {
   HttpException,
   HttpStatus,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { CreateTaskDto } from './create-task.dto';
 import { TaskService } from './tasks.service';
+import { ApiTags, ApiOperation, ApiResponse, ApiQuery } from '@nestjs/swagger';
 
+@ApiTags('tasks')
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly taskService: TaskService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Add a new task' })
+  @ApiResponse({})
   async create(@Body() createTaskDto: CreateTaskDto) {
     return await this.taskService.create(createTaskDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'List all tasks' })
   async getAll() {
     return await this.taskService.getAll();
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update a new task' })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Task not found' })
   async update(
     @Param('id', ParseIntPipe) id: number,
     @Body() createTaskDto: CreateTaskDto,
@@ -48,6 +56,9 @@ export class TasksController {
   }
 
   @Delete()
+  @ApiOperation({ summary: 'Delete one or many tasks' })
+  @ApiQuery({ name: 'ids', type: String, example: '1,2,3' })
+  @ApiResponse({ status: HttpStatus.NOT_FOUND, description: 'Task not found' })
   async delete(@Query('ids') id: string) {
     const ids = id
       .split(',')
