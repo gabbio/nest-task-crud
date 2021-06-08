c856356f7ac1d61b411b0a1bf4cc736ed182b451
Abou Bakr Gbané <aboubakr.gbane@gmail.com>
2021-06-08 14:39:26 +0000

    docs: add doc endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Task CRUD')
+    .setDescription('The Task API documentation')
+    .setVersion('1.0')
+    .addTag('tasks')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
